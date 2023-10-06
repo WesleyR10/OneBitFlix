c23a025b728bed945c1da0df2780423b22b329cf
WesleyR10 <wesleyribas2015@gmail.com>
2023-10-06 20:45:16 -0300

    Relationship between tables -create table courses

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,10 +1,15 @@
 import { ResourceWithOptions } from "adminjs";
-import { Category } from "../../models";
+import { Category, Course } from "../../models";
 import { categoryResourceOptions } from "./category";
+import { courseResourceOptions } from "./course";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Category, // Vem do models arquivo index
     options: categoryResourceOptions
-  }
+  },
+  {
+    resource: Course, // Vem do models arquivo index
+    options: courseResourceOptions
+  },
 ]
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,10 @@
 import { Category } from './Category'
+import { Course } from './Course'
+
+Category.hasMany(Course) // Uma categoria tem muitos cursos 
+Course.belongsTo(Category) // Um curso tem uma unica categoria
 
 export {
-  Category
+  Category,
+  Course
 }
