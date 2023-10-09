188d9e9590724b85150c69e0de7d7c482ea9d8fb
WesleyR10 <wesleyribas2015@gmail.com>
2023-10-09 19:40:48 -0300

    upload course covers

--- src/adminjs/resources/course.ts
+++ src/adminjs/resources/course.ts
@@ -1,4 +1,6 @@
-import { ResourceOptions } from "adminjs";
+import uploadFileFeature from "@adminjs/upload";
+import { FeatureType, ResourceOptions } from "adminjs";
+import path from "path";
 
 export const courseResourceOptions: ResourceOptions = {
   navigation: 'Catálogo',
@@ -7,3 +9,18 @@ export const courseResourceOptions: ResourceOptions = {
   listProperties: ['id', 'name', 'featured', 'categoryId'],
   showProperties: ['id', 'name', 'synopsis', 'featured', 'thumbnailUrl', 'categoryId', 'createdAt', 'updatedAt']
 }
+export const courseResourceFeatures: FeatureType[] = [  //Essa featureType vem direto do adminJs
+  uploadFileFeature({
+    provider: {
+      local: {
+        bucket: path.join(__dirname, '../../../public') // Pasta esta publica pois nao e um item necessario de proteção
+      }
+    },
+    properties: {
+      key: 'thumbnailUrl', // Coluna do nosso banco de dados onde vamos referenciar a capa 
+      file: 'uploadThumbnail'  // Representa o input do painel adm onde vamos fazer o upload
+    },
+    // Customizar o nome do arquivo e como vai ser salvo 
+    uploadPath: (record, filename) => `thumbnails/course-${record.get('id')}/${filename}` // Dentro da pasta public pasta especifica thumbnails - 
+  })
+]
